44b521810f6fba60bee278a71076a06d86b92e83
carolaine dos santos <carolainedossants@gmail.com>
2025-09-25 14:47:39 -0300

    feat: examples

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const express = require("express");
 const app = express();
 const porta = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Teste " + req.query.num);
+app.get("/api/:id", (req, res) => {
+  const id = req.params.id;
+  res.send("Hello Word e o numero draaag " + id);
 });
 
+// app.post("/", (req, res) => {
+//   res.send("Metodo POST");
+// });
+
 app.listen(porta, () => {
-  console.log("serviço rodando na porta " + porta);
+  console.log("Servidor rodando na porta " + porta);
 });
